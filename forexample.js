a4df7d6867a909d2756aa18ed455aa4585615723
ESCA7A <72895014+ESCA7A@users.noreply.github.com>
2020-10-15 02:05:34 +0300

    Add files via upload

---
+++ forexample.js
@@ -0,0 +1,10 @@
+"use Strict";
+let number1=2;
+let result;
+result=18+number1;
+result=result/number1;
+result=result*number1;
+result=result-number1;
+alert("пользовательский ввод = " +number1);
+alert("пустая = " +result);
+
